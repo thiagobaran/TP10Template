4ac52baff3c45531ffb2e6d4c20e40687fc9fad1
thiagobaran <106318788+thiagobaran@users.noreply.github.com>
2023-11-02 10:03:03 -0300

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-mui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Send from "@material-ui/icons/Send";
+
+// Importa los iconos de LinkedIn, Instagram y correo electrónico
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+
+const useStyles = makeStyles((theme) => ({
+  contactContainer: {
+    background: "#233",
+    height: "100vh",
+  },
+  heading: {
+    color: "tomato",
+    textAlign: "center",
+    textTransform: "uppercase",
+    marginBottom: "1rem",
+  },
+  form: {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    position: "absolute",
+  },
+  input: {
+    color: "#fff",
+  },
+  button: {
+    marginTop: "1rem",
+    color: "tomato",
+    borderColor: "tan",
+  },
+  field: {
+    margin: "1rem 0rem",
+  },
+  socialIcons: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "2rem",
+  },
+  socialIcon: {
+    margin: "0 1rem",
+    fontSize: "2rem",
+    color: "tomato",
+    cursor: "pointer",
+  },
+}));
+
+const InputField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "tomato",
+    },
+    "& label": {
+      color: "tan",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "tan",
+      },
+      "&:hover fieldset": {
+        borderColor: "tan",
+      },
+      "&.Mui-focused fieldset": {
+        color: "#fff",
+        borderColor: "tan",
+      },
+    },
+  },
+})(TextField);
+
+const Contact = () => {
+  const classes = useStyles();
+  return (
+    <Box component="div" className={classes.contactContainer}>
+      <Grid container justify="center">
+        <Box component="form" className={classes.form}>
+          <Typography variant="h5" className={classes.heading}>
+            Hire or Contact me...
+          </Typography>
+          <InputField
+            fullWidth={true}
+            label="Name"
+            variant="outlined"
+            inputProps={{ className: classes.input }}
+          />
+          <InputField
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+            inputProps={{ className: classes.input }}
+            className={classes.field}
+          />
+          <InputField
+            fullWidth={true}
+            label="Message"
+            variant="outlined"
+            multiline
+            rows={4}
+            inputProps={{ className: classes.input }}
+          />
+          <Button
+            variant="outlined"
+            fullWidth={true}
+            endIcon={<Send />}
+            className={classes.button}
+          >
+            Contact Me
+          </Button>
+          
+          {/* Botones de redes sociales */}
+          <div className={classes.socialIcons}>
+            <LinkedInIcon
+              className={classes.socialIcon}
+              onClick={() => window.open("URL de tu perfil de LinkedIn", "_blank")}
+            />
+            <InstagramIcon
+              className={classes.socialIcon}
+              onClick={() => window.open("URL de tu perfil de Instagram", "_blank")}
+            />
+          </div>
+        </Box>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Contact;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import creationsData from "../creations.json";
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    background: "#233",
+    height: "100%",
+  },
+  cardContainer: {
+    maxWidth: 345,
+    margin: "3rem auto",
+  },
+}));
+
+const Portfolio = () => {
+  const classes = useStyles();
+  const [projects, setProjects] = useState(creationsData);
+  const [favorites, setFavorites] = useState({});
+
+  useEffect(() => {
+    // Cargar favoritos desde localStorage al inicializar el componente
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+    if (storedFavorites) {
+      setFavorites(storedFavorites);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Guardar favoritos en localStorage cuando cambian
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const toggleFavorite = (title) => {
+    setFavorites((prevFavorites) => ({
+      ...prevFavorites,
+      [title]: !prevFavorites[title],
+    }));
+  };
+
+  return (
+    <Box component="div" className={classes.mainContainer}>
+      <Grid container justify="center">
+        {projects.map((project, i) => (
+          <Grid item xs={12} sm={8} md={4} key={i}>
+            <Card className={classes.cardContainer}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt={project.title}
+                  height="140"
+                  image={project.image}
+                />
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                    {project.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {project.description}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {project.date}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                {favorites[project.title] ? (
+                  <Button
+                    onClick={() => toggleFavorite(project.title)}
+                    size="small"
+                    color="secondary"
+                  >
+                    Remove from Favorites
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={() => toggleFavorite(project.title)}
+                    size="small"
+                    color="primary"
+                  >
+                    Add to Favorites
+                  </Button>
+                )}
+                <Button
+                  onClick={() => window.open(project.url, "_blank")}
+                  size="small"
+                  color="primary"
+                >
+                  Link
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Portfolio;
